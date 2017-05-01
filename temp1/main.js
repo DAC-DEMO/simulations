fea661ac0cdb4a31c04408d9831cc5b7a83ca29f
santosh <santosh.ece06@gmail.com>
2017-05-01 12:36:19 +0530

    cd

--- temp1/main.js
+++ temp1/main.js
@@ -3,10 +3,10 @@
  */
 var imageList = ["img/nodejs-vs-java.png", "img/141.png", "img/cool.jpg", "img/infinite_truth.jpg"];
 
-var contentList = ["Failureeee is simply the opportunity to begin again, this time more intelligently.",
+var contentList = ["Failure is simply the opportunity to begin again, this time more intelligently.",
     "Yesterday, you said tomorrow.",
     "If you want to go fast, go alone. If you want to go far, bring others along.",
-    "Alwayss remember that the future comes one day at a time.",
+    "Always remember that the future comes one day at a time.",
     "Don't find fault. Find a remedy.",
     "When we give up on our dreams, we die while still alive."
 ];
